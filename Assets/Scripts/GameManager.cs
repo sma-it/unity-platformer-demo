b94faedec3037fd21967cb45f63a51d7ef7b0093
yvan vander sanden <yvan@attr-x.net>
2023-10-23 21:29:11 +0200

    Started on Demo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton part
+    public static GameManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+            Destroy(this);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // gameManager part
+    private Player player;
+    [SerializeField] private GameObject spawnPoint;
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private bool gameActive = false;
+
+    private void Update()
+    {
+        if (!gameActive && Input.GetKeyDown(KeyCode.Space)) {
+            StartNewGame();
+        }
+    }
+
+    private void InstantiatePlayer()
+    {
+        player = Instantiate(playerPrefab).GetComponent<Player>();
+        player.transform.position = spawnPoint.transform.position;
+        FindObjectOfType<CameraFollower>().FocusOn(player);
+    }
+
+    public void StartNewGame()
+    {
+        if (spawnPoint == null)
+        {
+            Debug.Log("GameManager has no spawn point");
+        } else
+        {
+            InstantiatePlayer();
+
+            ScoreManager.instance.StartNewGame();
+            gameActive = true;
+        }
+    }
+
+    public void RespawnIfPossible()
+    {
+        if (ScoreManager.instance.Lives > 0)
+        {
+            InstantiatePlayer();
+        }
+    }
+}
